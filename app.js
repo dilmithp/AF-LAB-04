4fe7091bf766c0c1aa071215e74669b01fca9bf0
dilmithp <dilmithp@outlook.com>
2026-02-22 08:21:57 +0530

    using module

--- app.js
+++ app.js
@@ -32,3 +32,7 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
 }).on('error', (err) => {
   console.log("Error: " + err.message);
 });
+
+//using module
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
